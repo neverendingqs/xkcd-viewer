46f78b6226655db3c9bec78a7689c439a6feacf4
Mark Tse <mark.tse@neverendingqs.com>
2018-02-16 22:09:31 -0500

    app - added routing.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { bindActionCreators } from 'redux';
 
 import { comicRequest } from '../actions';
 import Comic from './Comic';
 
 class App extends Component {
-  componentWillMount() {
-    this.props.comicRequest();
-  }
-
   render() {
     return (
       <div className="container">
         <div className="text-center">
           <h1 className="display-1">XKCD Viewer</h1>
           <hr />
-          <Comic />
+          <BrowserRouter>
+            <div>
+              <Switch>
+                <Route path="/:id?" component={Comic} />
+              </Switch>
+            </div>
+          </BrowserRouter>
         </div>
       </div>
     );
