11e146419bf6ae0e163338c2083bff4f266954f5
Mark Tse <neverendingqs@users.noreply.github.com>
2018-02-20 12:46:45 -0500

    app - added navbar and about page. (#14)

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
+import About from '../components/About';
 import ComicRoute from '../containers/ComicRoute';
 
 class App extends Component {
@@ -8,11 +9,26 @@ class App extends Component {
     return (
       <div className="container">
         <div className="text-center">
-          <h1 className="display-1">XKCD Viewer</h1>
-          <hr />
           <BrowserRouter>
             <div>
+              <nav className="navbar navbar-expand-lg navbar-light navbar-transparent">
+                <Link className="navbar-brand h1" to="/">XKCD Viewer</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                  <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                      <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/about">About</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
               <Switch>
+                <Route path="/about" component={About}/>
                 <Route path="/:id?" component={ComicRoute} />
               </Switch>
             </div>
