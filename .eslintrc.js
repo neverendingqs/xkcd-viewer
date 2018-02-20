7085099bd57aac93486bd128d9f615ea446aca47
Mark Tse <neverendingqs@users.noreply.github.com>
2018-02-20 14:50:51 -0500

    ci - start linting and remove unused file. (#18)

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
       "react"
     ],
     "rules": {
-        "indent": ["error", 2],
+      "indent": ["error", 2, { "SwitchCase": 1 }],
         "linebreak-style": ["error", "unix"],
         "quotes": ["error", "single"],
         "react/jsx-uses-react": ["error"],
