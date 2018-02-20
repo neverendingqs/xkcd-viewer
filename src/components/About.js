70bc73a16f60192062f252e49e832d279951dec2
Mark Tse <mark.tse@neverendingqs.com>
2018-02-20 12:31:17 -0500

    app - added navbar and about page.

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+        <p>This website is purely for educational purposes only. See <a href="https://github.com/neverendingqs/xkcd-viewer" target="_blank" rel="noopener">neverendingqs/xkcd-viewer</a> for details.</p>
+        <p>XKCD is a comic series by Randall Munroe and is licensed under <a href="https://creativecommons.org/licenses/by-nc/2.5/" target="_blank" rel="noopener">CC BY-NC 2.5</a>.  Find out more about XKCD at <a href="https://xkcd.com/about/" target="_blank" rel="noopener">https://xkcd.com/about/</a> .</p>
+      </div>
+    );
+  }
+}
+
+export default About;
